6da725a2dd9fa6f40a852965aac631e63fb05e7a
Tarun Sharma <tarun.kr.sharma@hotmail.com>
2021-07-19 22:49:22 +0530

    Added ownable contract, also added level and cooldown property to zombie

--- contracts/ZombieFeeding.sol
+++ contracts/ZombieFeeding.sol
@@ -2,6 +2,7 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import "./CryptoZombieFactory.sol";
 
+
 contract KittyInterface {
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
@@ -19,11 +20,26 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  KittyInterface kittyContract = KittyInterface(ckAddress);
-  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public{
+  KittyInterface kittyContract;
+
+  function setKittyContractAddress(address _address) external onlyOwner{
+    kittyContract = KittyInterface(_address);
+  }
+
+  function _triggerCooldown(Zombie storage _zombie) internal{
+    _zombie.readyTime = uint32(now + cooldownTime);
+  }
+
+  function _isReady(Zombie storage _zombie) internal view returns(bool){
+    return (_zombie.readyTime <= now);
+  }
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal{
       require(msg.sender == zombieToOwner[_zombieId]);
       Zombie storage myZombie = zombies[_zombieId];
+
+require(_isReady(myZombie));
+
       _targetDna = _targetDna % dnaModulus;
       uint newDna = (myZombie.dna + _targetDna) / 2;
       
@@ -33,6 +49,7 @@ contract ZombieFeeding is ZombieFactory {
       }
       
       _createZombie("NoName", newDna);
+      _triggerCooldown(myZombie);
   }
 
   function feedOnKitty(uint _zombieId, uint _kittyId) public {
